13a3a1dd97a4d4b60f9833d8debae23758e00800
ImCrimP <pjmountin@gmail.com>
2024-02-27 15:59:02 -0600

    added uww image

--- portfolio/src/components/pages/About.jsx
+++ portfolio/src/components/pages/About.jsx
@@ -1,7 +1,16 @@
+import uww from "../../assets/uww.png";
+import "../../sass/About.scss";
 export default function About() {
   return (
     <div id="about">
-      <p>About</p>
+      <h1>About Me</h1>
+      <p className="about-text">
+        I am a front-end developer with a passion for creating beautiful and
+        functional websites. I have experience with HTML, CSS, JavaScript,
+        React, and SASS.
+      </p>
+      <h2>Education</h2>
+      <img id="uww" src={uww} alt="UW-Whitewater" />
     </div>
   );
 }
