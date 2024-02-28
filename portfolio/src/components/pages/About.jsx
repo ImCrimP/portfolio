dcd3733aecc29c49a19e59411dff67751ab402a4
ImCrimP <pjmountin@gmail.com>
2024-02-28 16:38:05 -0600

    added about section and education. projects sections looks good on ipad screen still need to add accessability to other sections incorporate mobile size too

--- portfolio/src/components/pages/About.jsx
+++ portfolio/src/components/pages/About.jsx
@@ -1,16 +1,31 @@
 import uww from "../../assets/uww.png";
+import family from "../../assets/family.png";
 import "../../sass/About.scss";
 export default function About() {
   return (
     <div id="about">
-      <h1>About Me</h1>
-      <p className="about-text">
-        I am a front-end developer with a passion for creating beautiful and
-        functional websites. I have experience with HTML, CSS, JavaScript,
-        React, and SASS.
-      </p>
-      <h2>Education</h2>
-      <img id="uww" src={uww} alt="UW-Whitewater" />
+      <h1 className="about-header">About Me</h1>
+      <div className="about-image-text">
+        <img id="family" src={family} alt="Peter Mountin" />
+        <p className="about-text">
+          I am a front-end developer with a passion for creating beautiful and
+          functional websites. I have experience with HTML, CSS, JavaScript,
+          React, and SCSS.
+        </p>
+      </div>
+
+      <h2 className="education-header">Education</h2>
+
+      <div className="education-container">
+        <img id="uww" src={uww} alt="UW-Whitewater" />
+        <div className="education-txt-container">
+          <span className="education-text">
+            University of Wisconsin - Whitewater, WI
+          </span>
+          <span className="education-text">BBA - General Business</span>
+          <span className="education-text">GPA - 3.872</span>
+        </div>
+      </div>
     </div>
   );
 }
